1502d9d5b53f932175b10b685ee69728238ca6f8
kaiden <kaiden@tokamak.network>
2024-08-20 11:11:53 +1000

    fix: test navigation error

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { LayoutComponent } from "components";
+import { LayoutComponent } from "components";
 import { PATH } from "consts";
 import { AirDropPage } from "pages";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      {/* <LayoutComponent> */}
-      <Routes>
-        <Route path={PATH.AIRDROP} element={<AirDropPage />} />
-      </Routes>
-      {/* </LayoutComponent> */}
+      <LayoutComponent>
+        <Routes>
+          <Route index element={<></>} />
+          <Route path={PATH.AIRDROP} element={<AirDropPage />} />
+        </Routes>
+      </LayoutComponent>
     </BrowserRouter>
   );
 };
